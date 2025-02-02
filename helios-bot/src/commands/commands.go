7fec0055c37e72402dc629d3dbc4012c0c45ef5e
ThatArekkusu <arekkusu5665@gmail.com>
2025-02-02 19:14:54 +0000

    Extra files

--- helios-bot/src/commands/commands.go
+++ helios-bot/src/commands/commands.go
@@ -4,16 +4,22 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-var (
-	integerOptionMinValue          = 1.0
-	dmPermission                   = false
-	defaultMemberPermissions int64 = discordgo.PermissionManageRoles
+// integerOptionMinValue          = 1.0
+// dmPermission                   = false
+// defaultMemberPermissions int64 = discordgo.PermissionManageRoles
 
-	commands = []*discordgo.ApplicationCommand{
-		{
-			Name:        "KickDate",
-			Type:        discordgo.ChatApplicationCommand,
-			Description: "Ban a user based on their account creation date",
+var commands = []*discordgo.ApplicationCommand{
+	{
+		Name:        "KickDate",
+		Type:        discordgo.ChatApplicationCommand,
+		Description: "Assigns how old a user must be to be kicked",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "date",
+				Description: "The date to kick users before",
+				Required:    true,
+			},
 		},
-	}
-)
+	},
+}
